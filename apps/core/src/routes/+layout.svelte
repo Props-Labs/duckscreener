992dc5f5858b06a687564f5805736756c3d8964e
bitcoinski <superrareuser@protonmail.com>
2024-11-13 16:28:36 -0500

    google analytics

--- apps/core/src/routes/+layout.svelte
+++ apps/core/src/routes/+layout.svelte
@@ -3,4 +3,16 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-8Z5T5S76DX"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-8Z5T5S76DX');
+	</script>
+</svelte:head>
+
 {@render children()}
