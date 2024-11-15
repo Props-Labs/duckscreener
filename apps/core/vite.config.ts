58a199430d1246813bf2d03c9a5d8c35142c196d
bitcoinski <superrareuser@protonmail.com>
2024-11-15 14:07:45 -0500

    fix: working chat

--- apps/core/vite.config.ts
+++ apps/core/vite.config.ts
@@ -1,40 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true,
+			},
+			protocolImports: true,
+		}),
+	],
 	ssr: {
-		noExternal: ['@fuels/*', 'svelte-fuels'],
-	},
-	resolve: {
-		alias: {
-			crypto: 'crypto-browserify',
-			stream: 'stream-browserify',
-			assert: 'assert',
-			util: 'util',
-			buffer: 'buffer',
-			process: 'process/browser',
-		}
+		noExternal: ['@fuels/*', 'svelte-fuels']
 	},
 	define: {
 		'process.env.NODE_DEBUG': 'false',
-		'global': 'globalThis',
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			define: {
-				global: 'globalThis'
-			}
-		},
-		include: [
-			'crypto-browserify',
-			'stream-browserify',
-			'assert',
-			'buffer',
-			'util',
-			'process/browser'
-		],
-		exclude: ['@fuels/*', 'svelte-fuels']
+		'global': 'globalThis'
 	}
 });
